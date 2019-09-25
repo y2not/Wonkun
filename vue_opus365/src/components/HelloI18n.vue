619309920763fc17c510371168e7c83808be3d39
Wonkun <jung.wonkun@gmail.com>
2019-09-25 21:26:32 +0900

    opus365 vue template

---
+++ vue_opus365/src/components/HelloI18n.vue
@@ -0,0 +1,17 @@
+<template>
+  <p class="display-1 font-weight-bold mb-2">{{ $t('hello') }}</p>
+</template>
+
+<script>
+export default {
+  name: 'HelloI18n'
+}
+</script>
+
+<i18n>
+{
+  "en": {
+    "hello": "Hello i18n in SFC!"
+  }
+}
+</i18n>
