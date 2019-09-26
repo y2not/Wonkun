edd8f77846a7ff6c7dfccb306078212bc392c7b2
Wonkun <jung.wonkun@gmail.com>
2019-09-27 07:38:56 +0900

    login vue file

---
+++ login_vue/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>OPUS</span>
+        <span class="font-weight-light">365</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <SignIn/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import SignIn from './components/SignIn';
+
+export default {
+  name: 'App',
+  components: {
+    SignIn,
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ login_vue/src/components/SignIn.vue
@@ -0,0 +1,113 @@
+  
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+            <v-card class="elevation-12">
+              <v-toolbar
+                color="primary"
+                dark
+                flat
+              >
+                <v-toolbar-title>Sign In  </v-toolbar-title>
+                <div class="flex-grow-1"></div>
+                <!-- <v-tooltip bottom>
+                  <template v-slot:activator="{ on }">
+                    <v-btn
+                      :href="source"
+                      icon
+                      large
+                      target="_blank"
+                      v-on="on"
+                    >
+                      <v-icon>mdi-code-tags</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Source</span>
+                </v-tooltip>
+                <v-tooltip right>
+                  <template v-slot:activator="{ on }">
+                    <v-btn
+                      icon
+                      large
+                      href="https://codepen.io/johnjleider/pen/pMvGQO"
+                      target="_blank"
+                      v-on="on"
+                    >
+                      <v-icon>mdi-codepen</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Codepen</span>
+                </v-tooltip> -->
+                <span>OPUS365</span>
+              </v-toolbar>  
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="User ID"
+                    name="useridS"
+                    prepend-icon="mdi-account"
+                    type="text"
+                  ></v-text-field>
+
+                  <v-text-field
+                    v-model="password"
+                    :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                    :rules="[rules.required, rules.min]"
+                    :type="show1 ? 'text' : 'password'"
+                    label="Password"
+                    hint="At least 8 characters"
+                    name="password"
+                    
+                    prepend-icon="mdi-lock"
+                    @click:append="show1 = !show1"
+                    counter
+                    
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <div class="flex-grow-1"></div>
+                <v-btn color="primary">Next</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    props: {
+      source: String,
+    },
+    data: () => ({
+        
+      drawer: null,
+      how1: false,
+      show2: true,
+      show3: false,
+      show4: false,
+      password: 'Password',
+      rules: {
+        required: value => !!value || 'Required.',
+        min: v => v.length >= 8 || 'Min 8 characters',
+        emailMatch: () => ('The email and password you entered don\'t match'),
+      },
+    }),
+  }
+</script>
