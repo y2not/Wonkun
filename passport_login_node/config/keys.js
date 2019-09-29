034f3b527955fe2c531d61e91d8471f7f304ed1a
Wonkun <jung.wonkun@gmail.com>
2019-09-29 18:37:23 +0900

    retry passport_login

---
+++ passport_login_node/config/keys.js
@@ -0,0 +1,6 @@
+// dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+dbPassword = 'mongodb://stormboy:7dnjsrms@ds011228.mlab.com:11228/useloginapp';
+
+module.exports = {
+    mongoURI: dbPassword
+};
